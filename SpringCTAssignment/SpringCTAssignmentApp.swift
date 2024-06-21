fc179e0e053b08b315efac34d22666c19ad1f782
RITWIKIOS <ritwikghosh22@gmail.com>
2024-06-21 12:14:33 +0530

    Initial Commit

---
+++ SpringCTAssignment/SpringCTAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpringCTAssignmentApp.swift
+//  SpringCTAssignment
+//
+//  Created by Moumita Paul Ghosh on 21/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SpringCTAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
